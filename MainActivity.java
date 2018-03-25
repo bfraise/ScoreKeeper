dcae500d6859959b6e8f6a4b58891bb9666d2ab0
bfraise <35715509+bfraise@users.noreply.github.com>
2018-03-25 00:08:52 -0500

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -1,75 +1,163 @@
-package com.example.android.scorekeeper;
+<?xml version="1.0" encoding="utf-8"?>
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
+    <RelativeLayout xmlns:android="http://schemas.android.com/apk/res/android"
+        android:layout_width="match_parent"
+        android:layout_height="match_parent"
+        android:background="@color/background_colors">
 
-public class MainActivity extends AppCompatActivity {
+        <LinearLayout
+            android:layout_width="match_parent"
+            android:layout_height="wrap_content"
+            android:orientation="horizontal"
+            android:showDividers="middle">
 
-    int scoreTeamA = 0;
-    int scoreTeamB = 0;
-    int resetScore = 0;
+            <LinearLayout
+                android:layout_width="0dp"
+                android:layout_height="wrap_content"
+                android:layout_weight="1"
+                android:gravity="center_horizontal"
+                android:orientation="vertical">
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+                <ImageView
+                    android:layout_width="64dp"
+                    android:layout_height="64dp"
+                    android:layout_marginTop="32dp"
+                    android:src="@drawable/generic_team_a" />
 
-    /**
-     * Displays methods for Team A.
-     */
-    public void addSixForTeamA(View v) {
-        scoreTeamA = scoreTeamA + 6;
-        displayForTeamA(scoreTeamA);
-    }
+                <EditText
+                    android:layout_width="wrap_content"
+                    android:layout_height="wrap_content"
+                    android:fontFamily="sans-serif-medium"
+                    android:hint="@string/team_name"
+                    android:inputType="text"
+                    android:text=""
+                    android:textColor="@color/team_name_colors"
+                    android:textSize="14sp" />
 
-    public void addThreeForTeamA(View v) {
-        scoreTeamA = scoreTeamA + 3;
-        displayForTeamA(scoreTeamA);
-    }
+                <TextView
+                    android:id="@+id/team_a_score"
+                    android:layout_width="wrap_content"
+                    android:layout_height="wrap_content"
+                    android:fontFamily="sans-serif-light"
+                    android:text="@string/zero"
+                    android:textColor="@color/score_colors"
+                    android:textSize="56sp" />
 
-    public void addOneForTeamA(View v) {
-        scoreTeamA = scoreTeamA + 1;
-        displayForTeamA(scoreTeamA);
-    }
+                <LinearLayout
+                    android:layout_width="wrap_content"
+                    android:layout_height="wrap_content"
+                    android:orientation="vertical">
 
-    public void displayForTeamA(int score) {
-        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
-        scoreView.setText(String.valueOf(score));
-    }
+                    <Button
+                        android:layout_width="match_parent"
+                        android:layout_height="0dp"
+                        android:layout_weight="1"
+                        android:onClick="addSixForTeamB"
+                        android:text="@string/touchdown"
+                        android:textColor="@color/button_colors" />
 
-    /**
-     * Displays methods for Team B.
-     */
-    public void addSixForTeamB(View v) {
-        scoreTeamB = scoreTeamB + 6;
-        displayForTeamB(scoreTeamB);
-    }
+                    <Button
+                        android:layout_width="match_parent"
+                        android:layout_height="0dp"
+                        android:layout_weight="1"
+                        android:onClick="addThreeForTeamA"
+                        android:text="@string/field_goal"
+                        android:textColor="@color/button_colors" />
 
-    public void addThreeForTeamB(View v) {
-        scoreTeamB = scoreTeamB + 3;
-        displayForTeamB(scoreTeamB);
-    }
+                    <Button
+                        android:layout_width="match_parent"
+                        android:layout_height="0dp"
+                        android:layout_weight="1"
+                        android:onClick="addOneForTeamA"
+                        android:text="@string/safety"
+                        android:textColor="@color/button_colors" />
 
-    public void addOneForTeamB(View v) {
-        scoreTeamB = scoreTeamB + 1;
-        displayForTeamB(scoreTeamB);
-    }
+                </LinearLayout>
 
-    public void displayForTeamB(int score) {
-        TextView scoreView = (TextView) findViewById(R.id.team_b_score);
-        scoreView.setText(String.valueOf(score));
-    }
+            </LinearLayout>
 
-    /**
-     * Displays methods for Reset Button.
-     */
-    public void resetScore(View v) {
-        scoreTeamA = 0;
-        scoreTeamB = 0;
-        displayForTeamA(scoreTeamA);
-        displayForTeamB(scoreTeamB);
-    }
-}
+            <View
+                android:layout_width="1dp"
+                android:layout_height="match_parent"
+                android:layout_centerHorizontal="true"
+                android:layout_marginTop="16dp"
+                android:background="@android:color/darker_gray"></View>
+
+            <LinearLayout
+                android:layout_width="0dp"
+                android:layout_height="wrap_content"
+                android:layout_weight="1"
+                android:gravity="center_horizontal"
+                android:orientation="vertical">
+
+                <ImageView
+                    android:layout_width="64dp"
+                    android:layout_height="64dp"
+                    android:layout_marginTop="32dp"
+                    android:src="@drawable/generic_team_b" />
+
+                <EditText
+                    android:layout_width="wrap_content"
+                    android:layout_height="wrap_content"
+                    android:fontFamily="sans-serif-medium"
+                    android:hint="@string/team_name"
+                    android:inputType="text"
+                    android:text=""
+                    android:textColor="@color/team_name_colors"
+                    android:textSize="14sp" />
+
+                <TextView
+                    android:id="@+id/team_b_score"
+                    android:layout_width="wrap_content"
+                    android:layout_height="wrap_content"
+                    android:fontFamily="sans-serif-light"
+                    android:text="@string/zero"
+                    android:textColor="@color/score_colors"
+                    android:textSize="56sp" />
+
+                <LinearLayout
+                    android:layout_width="wrap_content"
+                    android:layout_height="wrap_content"
+                    android:orientation="vertical">
+
+                    <Button
+                        android:layout_width="match_parent"
+                        android:layout_height="0dp"
+                        android:layout_weight="1"
+                        android:onClick="addSixForTeamB"
+                        android:text="@string/touchdown"
+                        android:textColor="@color/button_colors" />
+
+                    <Button
+                        android:layout_width="match_parent"
+                        android:layout_height="0dp"
+                        android:layout_weight="1"
+                        android:onClick="addThreeForTeamB"
+                        android:text="@string/field_goal"
+                        android:textColor="@color/button_colors" />
+
+                    <Button
+                        android:layout_width="match_parent"
+                        android:layout_height="0dp"
+                        android:layout_weight="1"
+                        android:onClick="addOneForTeamB"
+                        android:text="@string/safety"
+                        android:textColor="@color/button_colors" />
+
+                </LinearLayout>
+
+            </LinearLayout>
+
+        </LinearLayout>
+
+        <Button
+            android:layout_width="wrap_content"
+            android:layout_height="wrap_content"
+            android:layout_alignParentBottom="true"
+            android:layout_centerHorizontal="true"
+            android:layout_marginBottom="32dp"
+            android:onClick="resetScore"
+            android:text="@string/reset"
+            android:textColor="@color/button_colors" />
+
+    </RelativeLayout>
